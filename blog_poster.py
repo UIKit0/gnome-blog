0127b49f66d3024445b78e4cebeaa1669439ba1f
Seth Nickell <seth@src.gnome.org>
2003-08-29 09:55:53 +0000

    #(*$&*

--- blog_poster.py
+++ blog_poster.py
@@ -60,7 +60,7 @@ class BlogPoster(gtk.Frame):
         box.show_all()
 
  
-    def _postEntry (self, username, password, blogid, url, text):
+    def _postEntry (self, username, password, blog_id, url, text):
         if (url == None):
             self.reportError("Could not post Blog entry", "No XML-RPC server URL to post blog entries to is set, or the value could not be retrieved from GConf. Your entry will remain in the blogger window.")
             return gtk.FALSE
@@ -71,7 +71,7 @@ class BlogPoster(gtk.Frame):
 
         try:
             server.blogger.newPost(appkey, blog_id, username, password,
-                                   entryText, 1)
+                                   text, 1)
         except xmlrpclib.Fault, e:
             primary = "Could not post Blog entry"
             if (e == 'Method Error'):
